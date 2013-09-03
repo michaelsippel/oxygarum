1e2f78b6cdf3fc0e7557a9d0bc16c534af10f62d
Micha <micha.linuxfreak@googlemail.com>
2013-09-03 19:48:05 +0200

    first particle header

---
+++ include/particle.h
@@ -0,0 +1,46 @@
+/**
+ *  include/particle.h
+ *
+ *  (C) Copyright 2013 Michael Sippel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _PARTICLE_H
+#define _PARTICLE_H
+
+#include "vertex.h"
+#include "vector.h"
+#include "material.h"
+
+typedef struct particle {
+  vertex3d_t pos;
+  vector3d_t velocity;
+  unsigned int lifetime;
+  unsigned int age;
+  unsigned int fade_in;
+  unsigned int fade_out;
+  color_t color;
+} particle_t;
+
+typedef struct particle_emiter {
+  vertex3d_t pos;
+  vertex3d_t gravity;
+  unsigned int max_particles;
+  unsigned int num_particles;
+  unsigned int num_particles_per_emision;
+  unsigned int emision_rate;
+} particle_emiter_t;
+
+#endif
+
