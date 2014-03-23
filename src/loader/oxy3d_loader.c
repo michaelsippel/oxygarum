3449c8ae5091181aea96bef6c3d36349a965440b
Micha <michamimosa@gmail.com>
2014-03-23 23:47:14 +0100

    auto-count arguments

--- src/loader/oxy3d_loader.c
+++ src/loader/oxy3d_loader.c
@@ -19,6 +19,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 
 #include "vertex.h"
 #include "face.h"
@@ -28,11 +30,17 @@
 #include "group.h"
 #include "oxygarum.h"
 
-void readstr(FILE *f, char *string) {
+int readstr(FILE *f, char *string) {
+    int i = 0;
     do {
-	fgets(string, 255, f);
+	if(fgets(string, 255, f) == NULL) {
+          return -1;
+        }
     } while ((string[0] == '#') || (string[0] == '\n'));
-    return;
+    int len = strlen(string);
+    string[len-1] = 0;
+    printf("read : %s\n", string);
+    return 0;
 }
 /*
 struct str_enum {
@@ -73,6 +81,23 @@ GLenum gl_str2enum(char *str) {
   return a;
 }
 */
+
+int count_arguments(FILE *f) {
+  int a = 0;
+  char line[256];
+  char second;
+
+  int pos = ftell(f);
+  do {
+    readstr(f, line);
+    second = line[1];
+    a++;
+  } while(second != ' ');
+  fseek(f, pos, SEEK_SET);  
+
+  return a;
+}
+
 struct load_return *oxygarum_load_oxy3d_file(const char *path) {
   FILE *f = fopen(path, "rt");
   if(!f) {
@@ -87,32 +112,39 @@ struct load_return *oxygarum_load_oxy3d_file(const char *path) {
   ret->objects = NULL;
   
   char line[256];
-
-int i;
-#define FOR_SUB_CMDS(a) \
-  for(i = 0; i < a; i++) { \
-  readstr(f, line); \
-  sub_cmd = line[0]; \
-  strcpy(params, &line[2]);
+  int i;
+  
+#define FOR_SUB_CMDS \
+  num_args = count_arguments(f); \
+  printf("[LOAD] %d arguments\n", num_args);\
+  for(i = 0; i < num_args; i++) { \
+    readstr(f, line); \
+    sub_cmd = line[0]; \
+    strcpy(params, &line[2]);
   
   char cmd[3] = "cmd";
   char sub_cmd;
   char name[256];
   char params[256];
-  while(1) {
-    readstr(f, line);
+  int pos = 0;
+  int num_args = 0;  
+  
+  while(readstr(f, line) == 0) {
     memcpy(cmd, line, 3);
     strcpy(name, &line[4]);
+    
     if(strcmp(cmd, "inc") == 0) {
       struct load_return *inc = oxygarum_load_oxy3d_file(name);
       oxygarum_group_join(ret->textures, inc->textures);
       oxygarum_group_join(ret->materials, inc->materials);
       oxygarum_group_join(ret->meshes, inc->meshes);
       oxygarum_group_join(ret->objects, inc->objects);
+      printf("[LOAD] include %s\n", name);
     } else if(strcmp(cmd, "tex") == 0) {
       char path[256];
       //GLenum minfilter, magfilter;
-      FOR_SUB_CMDS(1)
+      FOR_SUB_CMDS
+        printf("sub cmd: %c\n", sub_cmd);
         switch(sub_cmd) {
           case 'p':
             strcpy(path, params);
@@ -125,6 +157,7 @@ int i;
             break;*/
 	}
       }
+      printf("[LOAD] texture %s:\n\tpath: %s\n", name, &path);
       texture_t *tex = oxygarum_load_texture_from_file(path, NULL);
       oxygarum_group_add(ret->textures, (void*) tex, name);
     } else if(strcmp(cmd, "mat") == 0) {
@@ -135,7 +168,7 @@ int i;
       char c_tex_name[256];
       group_entry_t *t_entry;
       
-      FOR_SUB_CMDS(2)
+      FOR_SUB_CMDS
         switch(sub_cmd) {
           case 'c':
             sscanf(params, "%2x%2x%2x %f %s", &r, &g, &b, &mat->color.color[3], &c_tex_name);
@@ -155,16 +188,31 @@ int i;
       }
       oxygarum_group_add(ret->materials, (void*) mat, name);
     } else if(strcmp(cmd, "msh") == 0) {
-      int num_vertices = 8;
-      int num_uvmaps = 1;
-      int num_faces = 6;
+      int num_vertices = 0;
+      int num_normals = 0;
+      int num_uvmaps = 0;
+      int num_texcoords = 0;
+      int num_faces = 0;
       
-      FOR_SUB_CMDS(18)
+      // count vertices
+      pos = ftell(f);
+      FOR_SUB_CMDS
         switch(sub_cmd) {
           case 'v':
+            num_vertices ++;
+            break;
+          case 'n':
+            num_normals ++;
+            break;
+          case 't':
+            num_texcoords ++;
+            break;
+          case 'f':
+            num_faces ++;
             break;
         }
       }
+      // TODO
     } else if(strcmp(cmd, "obj") == 0) {
       // TODO
     }
